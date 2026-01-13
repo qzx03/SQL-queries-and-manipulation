6236e5cb7a364430a06265bbbf2ca8d8fad66686
ahmed <ahmedelmorabiti123460@gmail.com>
2026-01-13 10:26:16 +0100

    added inserts

---
+++ Insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO user (name, email, password)
+VALUES ('Alice', 'alice@test.com', '1234');
+
+INSERT INTO Article (title, content, P_DATE, user_id)
+VALUES ('welcome to the blog', 'this is the first article.', '2025-07-18', 1);
+
+INSERT INTO Utilisateur (name, email, password)
+VALUES 
+  ('tarik', 'tarik@gmail.com', 'pass123'),
+  ('alae', 'alae@outloook.com', '12345678');
+
+SELECT * FROM user;
+SELECT * FROM Article;
